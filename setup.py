a27ca4f343bb230a07c1e5b83b0650b4fd874f9a
lvxejay <jared@onelvxe.com>
2018-01-11 15:48:02 -0600

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='bpynodes_lib',
+      version='0.0.1',
+      description='Blender Python Nodes extension that provides a python library for creating Python Node systems',
+      url='',
+      author='Jared Webber',
+      author_email='',
+      license='GNU GPLv3',
+      packages=find_packages(),
+      #install_requires=['setuptools', 'wheel', 'requests', 'numpy'],
+      zip_safe=False)
