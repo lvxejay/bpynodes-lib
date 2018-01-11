5cf0df9650c6f0dc5b812f46ba7586a57c8576a7
lvxejay <jared@onelvxe.com>
2018-01-11 17:06:03 -0600

    Updated release to 1.0.1. Adding PyPi configuration and distribution files.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='bpynodes_lib',
-      version='0.0.1',
+      version='1.0.1',
       description='Blender Python Nodes extension that provides a python library for creating Python Node systems',
-      url='',
+      url='https://gitlab.com/blenderone/bpynodes_lib1',
       author='Jared Webber',
-      author_email='',
+      author_email='info@onelvxe.com',
       license='GNU GPLv3',
-      packages=find_packages(),
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      keywords='blender bpy nodes custom pynodes',
+      python_requires='~=3.5',
       #install_requires=['setuptools', 'wheel', 'requests', 'numpy'],
       zip_safe=False)
